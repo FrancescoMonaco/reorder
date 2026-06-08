c9bd879b38e122a1ef7d3486d8889b33e07bf1e1
FrancescoMonaco <francescopio.monaco@unitn.it>
2026-06-08 09:53:53 +0200

    Add python binding

---
+++ python/wrapper.cpp
@@ -0,0 +1,11 @@
+#include <memory>
+#include <nanobind/nanobind.h>
+#include <nanobind/ndarray.h>
+#include <nanobind/stl/string.h>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+#include "reorder/reorder.hpp"
+
+namespace nb = nanobind;
